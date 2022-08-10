34314f7c7d41eef27e9f5720cf116e960ff5800f
lucasrique17 <henrique-llucas@hotmail.com>
2022-08-09 23:34:09 -0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import { onAuthStateChanged } from 'firebase/auth';
+
+// HOOKS
+import { useState, useEffect } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
+// CONTEXT
+import { AuthProvider } from './context/AuthContext';
+
+// PAGES
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
+import Preview from './pages/Preview/Preview';
+
+
+// COMPONENTES
+import NavBar from './components/Navbar'
+import Footer from './components/Footer'
+
+
 function App() {
+
+  const [user, setUser] = useState(undefined)
+  const {auth} = useAuthentication()
+
+  //Fazer Login
+  const loadingUser = user === undefined
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      setUser(user)
+    })
+
+  }, [auth])
+  
+  if(loadingUser) {
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <AuthProvider value={{user}}>
+      <BrowserRouter>
+      <NavBar />
+      <div className='container'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
+          <Route path='/register' element={!user ? <Register /> : <Navigate to='/' />} />
+          <Route path='/preview' element={user ? <Preview /> : <Navigate to='/login' />} />
+        </Routes>
+      </div>
+      <Footer />
+      </BrowserRouter>
+     </AuthProvider>
     </div>
   );
 }
