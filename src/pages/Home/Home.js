3288b7112103cda487632dfd6363a84a468ab5be
lucasrique17 <henrique-llucas@hotmail.com>
2022-08-10 15:03:21 -0300

    update home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,8 +5,9 @@ import styles from './Home.module.css'
 
 const Home = () => {
   return (
-      <div className={styles.container_home}>
-        <h1>Home</h1>
+      <div className={styles.home}>
+        <h1>Seja bem vindo ao Preview</h1>
+        <p>O site com as melhores tendências de mercado!!!</p>
       </div>
   )
 }
