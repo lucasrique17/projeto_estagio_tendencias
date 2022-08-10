d70ed09451f1edd0001a60f68dd07303183daea5
lucasrique17 <henrique-llucas@hotmail.com>
2022-08-09 23:41:44 -0300

    update about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,7 +7,8 @@ const About = () => {
   return (
     <div className={styles.about}>
         <h2>Sobre o projeto</h2>
-        <p>Este projeto consiste em....</p>
+        <p>Este projeto tem como objetivo realizar uma análise de cotações de valores do preço diário da ação da Petrobrás. Através de um algoritmo feito em Python, que analisa uma grande base temporal de preços para que chegue ao valor mais aproximado.</p>
+        <p>Projeto desenvolvido Lucas Henrique Soares Cruz e André Augusto de Almeida Machado, alunos da Universidade Estadual do Estado de Minas Gerais (UEMG), campus Frutal - Minas Gerais.</p>
         <Link to='/login' className='btn'>Faça Parte</Link>
     </div>
   )
